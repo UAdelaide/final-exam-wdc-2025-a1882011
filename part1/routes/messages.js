b4cc6a51bc3a22b5eb31e92fe5744c31ad00bfde
Hyuna Kim <a1882011@adelaide.edu.au>
2025-06-20 09:24:29 +0000

    [automatic commit] 2025-06-20 09:24:29 UTC 121.45.86.50

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -34,7 +34,8 @@ router.get('/walkers/summary', async(req, res) => {
         SELECT U.username AS walker_username,
                COUNT(r.rating_id) AS total_ratings,
                AVG(r.rating) AS average_rating,
-               COUNT(wr.request_id) AS completed_walks From Users U
+               COUNT(wr.request_id) AS completed_walks
+        From Users U
         JOIN WalkRequests wr ON U.user_id = wr.walker_id
         JOIN WalkRatings r on wr.request_id = r.request_id
         WHERE wr.status = 'completed'
