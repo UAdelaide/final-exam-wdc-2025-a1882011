9325df5077b73f1407c0d31f91a7e8698c1f6552
Hyuna Kim <a1882011@adelaide.edu.au>
2025-06-20 03:48:57 +0000

    [automatic commit] 2025-06-20 03:48:57 UTC ;; communications error to 208.67.222.222#53: timed out 121.45.86.50

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -12,6 +12,7 @@ router.get('/dogs',async(req, res) => {
 });
 
 router.get('/walkrequests/open',async(req, res) => {
-    const[rows] = await db.query()
+    const[rows] = await db.query(`
+        SELECT `)
 })
 module.exports = router;
