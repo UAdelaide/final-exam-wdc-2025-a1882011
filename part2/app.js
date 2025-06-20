385fad1975a8301c36d22a943fa8f66d8fa90f5a
Hyuna Kim <a1882011@adelaide.edu.au>
2025-06-20 16:48:03 +0000

    [automatic commit] 2025-06-20 16:48:03 UTC 121.45.86.50

--- part2/app.js
+++ part2/app.js
@@ -13,6 +13,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 const dogRoutes = require('./routes/dogRoutes');
+const dogRoutes = require('./routes/dogRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
