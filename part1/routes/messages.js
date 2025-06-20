368d9daa94486097a7d894ee1315aeeb9bff081b
Hyuna Kim <a1882011@adelaide.edu.au>
2025-06-20 04:40:49 +0000

    [automatic commit] 2025-06-20 04:40:49 UTC 121.45.86.50

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -33,6 +33,6 @@ router.get('/walkers/summary', async(req, res) => {
     const[rows] = await db.query(`
         SELECT Users.username AS walker_username From WalkRatings,
         SELECT COUNT(rating.rating_id) AS total_ratings,
-        SELECT A`)
+        SELECT AVG()`)
 })
 module.exports = router;
