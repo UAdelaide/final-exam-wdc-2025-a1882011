8483c3870ad5326082742a51d8c39376e9308638
Hyuna Kim <a1882011@adelaide.edu.au>
2025-06-20 09:29:27 +0000

    [automatic commit] 2025-06-20 09:29:27 UTC 121.45.86.50

--- part1/db.js
+++ part1/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    passowrd: '',
+    password: '',
     database: 'DogWalkService'
 });
 module.exports = db;
