4dc857fcc2fc8ecd60c244d40759c01f6a68ca55
Hyuna Kim <a1882011@adelaide.edu.au>
2025-06-20 03:40:24 +0000

    [automatic commit] 2025-06-20 03:40:24 UTC 121.45.86.50

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -2,9 +2,6 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-var
-var
-
 router.get('/items',async(req, res) => {
     const [rows] =await db.query(`
         SELECT Dogs.name AS dog_name,
