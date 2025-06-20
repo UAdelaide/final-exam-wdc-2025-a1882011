c94ed983d8a4ad127c5b7acdfe2656ded0350233
Hyuna Kim <a1882011@adelaide.edu.au>
2025-06-20 15:09:06 +0000

    [automatic commit] 2025-06-20 15:09:06 UTC 121.45.86.50

--- part2/app.js
+++ part2/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-//add form for tras
+//add to transger form
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
 
