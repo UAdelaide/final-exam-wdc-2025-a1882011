fd3277c35e95348f2f9a12ec527553d2d274fb53
Hyuna Kim <a1882011@adelaide.edu.au>
2025-06-20 04:28:48 +0000

    [automatic commit] 2025-06-20 04:28:48 UTC 121.45.86.50

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -25,6 +25,6 @@ router.get('/walkrequests/open',async(req, res) => {
 router.post('/messages', async(req, res) => {
     const {request_id, walker_id, owner_id, rating, comments} = req.body;
     await db.query(`INSERT INTO WalkRating(reques_id, walker_id, owner_id, rating, comments)
-        VALUES(?,?,?,?,?), [request_id, walker_id, owner_id, rating, comments]);`)
+        VALUES(?,?,?,?,?)`,)
 });
-module.exports = router;
+module.exports = router
