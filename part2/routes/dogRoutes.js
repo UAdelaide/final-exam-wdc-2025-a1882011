a07507dd109177d5f7491824b4d2bd5445e5f91e
Hyuna Kim <a1882011@adelaide.edu.au>
2025-06-21 02:32:09 +0000

    [automatic commit] 2025-06-21 02:32:09 UTC 121.45.86.50

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
 router.get('/dogsname', async(req, res) => {
   const userid = req.session.user?.user_id:
   try {
-    const [rows] = await db.query('SELECT dog_id, name FROM Dogs');
+    const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?', [userId]);
     res.json(rows);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch users' });
