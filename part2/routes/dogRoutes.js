757eaa54792bb210816a7dc13b47308eb04b0b4b
Hyuna Kim <a1882011@adelaide.edu.au>
2025-06-21 03:14:27 +0000

    [automatic commit] 2025-06-21 03:14:26 UTC 121.45.86.50

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
 });
 //add dog name
 router.get('/dogname', async(req, res) => {
-  const userid = req.session?.user_id;
+  const userid = req.query.user_id;
   console.log('userid from session:', userid);
 
   try{
