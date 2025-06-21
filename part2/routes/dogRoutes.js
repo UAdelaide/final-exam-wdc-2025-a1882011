80a15e13b860b1d8fc6cdf2ffc1b6a6e6f883baa
Hyuna Kim <a1882011@adelaide.edu.au>
2025-06-21 02:34:13 +0000

    [automatic commit] 2025-06-21 02:34:13 UTC 121.45.86.50

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/dogsname', async(req, res) => {
-  const userid = req.session.user?.user_id:
+  const userid = req.session.user?.user_id;
   try {
     const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?',[userid]);
     res.json(rows);
