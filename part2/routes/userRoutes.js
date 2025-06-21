7f8b893d860df3bc254442f80bae75cde210344b
Hyuna Kim <a1882011@adelaide.edu.au>
2025-06-21 03:44:51 +0000

    [automatic commit] 2025-06-21 03:44:51 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out 121.45.86.50

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -48,7 +48,7 @@ router.post('/login', async (req, res) => {
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
-    //save user info
+ //save user info
     req.session.user = rows[0];
 
     res.json({ message: 'Login successful', user: rows[0] });
